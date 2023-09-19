85f43a9cc7fd9ea626cca8738dc3dfc50e137410
NilEis <52820950+NilEis@users.noreply.github.com>
2023-09-19 22:18:26 +0200

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/static"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    speedInsights: { enabled: true }
+  })
 });
