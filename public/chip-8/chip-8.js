a9618d84d5d769852b667c7b59c6c012b58917cc
NilEis <52820950+NilEis@users.noreply.github.com>
2023-09-20 19:48:57 +0200

    does it work now

--- public/chip-8/chip-8.js
+++ public/chip-8/chip-8.js
@@ -51,33 +51,6 @@ function GROWABLE_HEAP_F64() {
 
 var Module = typeof Module != "undefined" ? Module : {};
 
-if (typeof SharedArrayBuffer === "undefined") {
- const dummyMemory = new WebAssembly.Memory({
-  initial: 0,
-  maximum: 0,
-  shared: true
- });
- globalThis.SharedArrayBuffer = dummyMemory.buffer.constructor;
-}
-
-Module.print = (function() {
- var element = document.getElementById("output");
- if (element) element.value = "";
- return function(text) {
-  if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(" ");
-  console.log(text);
-  if (element) {
-   element.value += text + "\\n";
-   element.scrollTop = element.scrollHeight;
-  }
- };
-})();
-
-Module.canvas = (function() {
- var canvas = document.getElementById("canvas");
- return canvas;
-})();
-
 var moduleOverrides = Object.assign({}, Module);
 
 var arguments_ = [];
