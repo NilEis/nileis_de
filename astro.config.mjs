eebeea97d0cda1a304fffa833184890e847fe611
NilEis <52820950+NilEis@users.noreply.github.com>
2023-09-20 15:05:27 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
-import vercel from "@astrojs/vercel/static"
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://www.nileis.de",
+  integrations: [react(), sitemap()],
   adapter: vercel({
-    webAnalytics: { enabled: true },
-    speedInsights: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    },
+    speedInsights: {
+      enabled: true
+    }
   })
 });
