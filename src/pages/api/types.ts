cb68a09a6cdc4a60626fb5bd73246f51b63fd0f4
NilEis <52820950+NilEis@users.noreply.github.com>
2024-05-24 12:18:09 +0200

    add: version api

---
+++ src/pages/api/types.ts
@@ -0,0 +1,1 @@
+export type VersionResult = {hash: string, name: string} ;
---
+++ src/pages/api/version.json.ts
@@ -0,0 +1,17 @@
+import type {APIRoute} from 'astro';
+import childProcess from 'child_process';
+import type { VersionResult } from './types';
+
+const version: VersionResult= {
+  hash: childProcess.execSync('git rev-parse --short HEAD').toString().trim(),
+  name: childProcess.execSync('git log -1 --oneline')
+            .toString()
+            .split(' ')
+            .slice(1)
+            .join(' ')
+            .trim()
+};
+
+export const GET: APIRoute = ({params, request}) => {
+  return new Response(JSON.stringify(version));
+}
