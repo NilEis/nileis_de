2a00159a940df1229d019afc5149f4167fcb75c6
NilEis <52820950+NilEis@users.noreply.github.com>
2024-11-01 21:16:45 +0100

    add: sonderzeichen converter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/static';
 import {defineConfig} from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://www.nileis.de',
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), tailwind()],
   adapter:
       vercel({webAnalytics: {enabled: true}, speedInsights: {enabled: true}})
 });
