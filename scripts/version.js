988e5e9a3196c7e21b639f689f2924e7c4d701c9
NilEis <52820950+NilEis@users.noreply.github.com>
2025-02-08 21:26:18 +0100

    fix: version not prerendered

---
+++ scripts/version.js
@@ -0,0 +1,17 @@
+import { execSync } from "child_process";
+import { writeFileSync } from "fs";
+import { join } from "path";
+
+const version = {
+  hash: execSync("git rev-parse --short HEAD").toString().trim(),
+  name: execSync("git log -1 --oneline")
+          .toString()
+          .split(" ")
+          .slice(1)
+          .join(" ")
+          .trim(),
+};
+
+const outPath = join(process.cwd(), "src", "pages", "components", "version.json");
+writeFileSync(outPath, JSON.stringify(version, null, 2));
+console.log("✅ Generated version file:", version);
