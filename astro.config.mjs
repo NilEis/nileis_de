ac5aa741361e8cfc90586d189f0e8a93afa87fa6
NilEis <52820950+NilEis@users.noreply.github.com>
2025-02-08 20:42:18 +0100

    add: custom background images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
-import {defineConfig} from 'astro/config';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://www.nileis.de',
+  output: "hybrid",
+  site: "https://www.nileis.de",
   integrations: [react(), sitemap(), tailwind()],
-  adapter:
-      vercel({webAnalytics: {enabled: true}, speedInsights: {enabled: true}})
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    speedInsights: { enabled: true },
+    maxDuration: 1
+  }),
 });
