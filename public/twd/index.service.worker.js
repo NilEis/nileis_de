10cc20af1ebd6b80db43888fe0dbab76cb18dc30
NilEis <52820950+NilEis@users.noreply.github.com>
2025-03-04 19:47:10 +0100

    update: twd

--- public/twd/index.service.worker.js
+++ public/twd/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741112778|126854172';
+const CACHE_VERSION = '1741113966|628637550';
 /** @type {string} */
 const CACHE_PREFIX = 'AutomaticTowerDe-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
