e8aafbcc92bb36ad01d1f9b326df51ecb9e88e64
NilEis <52820950+NilEis@users.noreply.github.com>
2025-06-24 02:03:17 +0900

    ref: update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,23 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import compressor from "astro-compressor";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://www.nileis.de",
-  integrations: [react(), sitemap(), tailwind(), compressor()],
+  integrations: [react(), sitemap(), compressor()],
+
   adapter: vercel({
     webAnalytics: { enabled: true },
     speedInsights: { enabled: true },
     maxDuration: 1,
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
