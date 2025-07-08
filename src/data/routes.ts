a1cc82dee2b4c33f4e168107f2fbf650789f7d7f
NilEis <52820950+NilEis@users.noreply.github.com>
2025-07-09 01:00:52 +0200

    update routes

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -40,19 +40,19 @@ export const routes: Route[] = generateRoutes([
     url: 'twd/',
     reload: true,
     Description:
-    'An automatic tower defence game. This page hosts a browser-based game called "Automatic Tower Defence," built using the Godot engine. It features a canvas for rendering the game and includes a status overlay that displays a loading progress bar or error messages if required features are missing. The page ensures compatibility by checking for necessary browser features and attempts to resolve issues, such as registering a service worker if needed. Once everything is ready, the game starts and provides an immersive, fullscreen experience.'
+        'An automatic tower defence game. This page hosts a browser-based game called "Automatic Tower Defence," built using the Godot engine. It features a canvas for rendering the game and includes a status overlay that displays a loading progress bar or error messages if required features are missing. The page ensures compatibility by checking for necessary browser features and attempts to resolve issues, such as registering a service worker if needed. Once everything is ready, the game starts and provides an immersive, fullscreen experience.'
   },
   {
     name: 'Wave function collapse',
     url: 'wfc/',
     Description:
-    'This page showcases a Wave Function Collapse (WFC) algorithm visualization. It features an interactive canvas where users can observe the algorithm in action, generating patterns or solving constraints dynamically. The page is wrapped in a clean layout and ensures compatibility with modern browsers, providing an engaging experience for exploring procedural generation techniques.'
+        'This page showcases a Wave Function Collapse (WFC) algorithm visualization. It features an interactive canvas where users can observe the algorithm in action, generating patterns or solving constraints dynamically. The page is wrapped in a clean layout and ensures compatibility with modern browsers, providing an engaging experience for exploring procedural generation techniques.'
   },
   {
     name: 'RegexToNFA',
     url: 'regex/',
     Description:
-    'A regular expression to NFA converter. It converts a regular expression to a non-deterministic finite automaton and then to a deterministic finite automaton. Both are displayed as graphs. It is written in C#'
+        'A regular expression to NFA converter. It converts a regular expression to a non-deterministic finite automaton and then to a deterministic finite automaton. Both are displayed as graphs. It is written in C#'
   },
   {
     name: 'Voxel space',
@@ -74,25 +74,25 @@ export const routes: Route[] = generateRoutes([
     name: 'Sonderzeichen',
     url: 'sonderzeichen/',
     Description:
-    'A tool to convert special characters to a special sonderzeichen format. It replaces characters with similar looking characters'
+        'A tool to convert special characters to a special sonderzeichen format. It replaces characters with similar looking characters'
   },
   {
     name: 'Hackertype',
     url: 'hackertype/',
     Description:
-    'A tool that prints source code in a hacker style. It is a joke.'
+        'A tool that prints source code in a hacker style. It is a joke.'
   },
   {
     name: 'Kruskal minimum spanning tree',
     url: 'kruskal/',
     Description:
-    'A tool to visualize the Kruskal algorithm for finding the minimum spanning tree of a graph.'
+        'A tool to visualize the Kruskal algorithm for finding the minimum spanning tree of a graph.'
   },
   {
     name: 'Whatsapp backup viewer',
     url: 'chat_viewer/',
     Description:
-    'A tool to view whatsapp chat backups. It can display the chat, sounds, images and videos'
+        'A tool to view whatsapp chat backups. It can display the chat, sounds, images and videos'
   },
   {
     name: 'AI',
@@ -101,10 +101,10 @@ export const routes: Route[] = generateRoutes([
       name: 'Image to Latex',
       url: 'image_to_latex/',
       Description:
-      'This page allows users to upload an image containing mathematical expressions, which is then converted into LaTeX code. The page displays the uploaded image, the generated LaTeX code, and a rendered preview of the mathematical expressions. It includes a loading animation while processing the image and provides a clean, interactive interface for converting images to LaTeX.'
+          'This page allows users to upload an image containing mathematical expressions, which is then converted into LaTeX code. The page displays the uploaded image, the generated LaTeX code, and a rendered preview of the mathematical expressions. It includes a loading animation while processing the image and provides a clean, interactive interface for converting images to LaTeX.'
     }],
     Description:
-    'This page is a directory for AI-related tools or resources. It features a clean and simple design with a full-screen layout. The page includes a list of links, such as "Image to Latex," which directs users to specific AI tools or functionalities. The links are organized alphabetically for easy navigation.'
+        'This page is a directory for AI-related tools or resources. It features a clean and simple design with a full-screen layout. The page includes a list of links, such as "Image to Latex," which directs users to specific AI tools or functionalities. The links are organized alphabetically for easy navigation.'
   },
   {
     name: 'Asteroid',
@@ -115,16 +115,17 @@ export const routes: Route[] = generateRoutes([
   {
     name: 'Search',
     url: 'search/',
+    reload: true,
     Description:
-    'This page provides a vector-based search functionality. Users can input a search term into the search bar, and the page dynamically retrieves and displays the most relevant results based on semantic similarity. The results are ranked using a cosine distance metric and link to related resources. It offers an interactive and efficient way to explore content using advanced embedding techniques.'
+        'This page provides a vector-based search functionality. Users can input a search term into the search bar, and the page dynamically retrieves and displays the most relevant results based on semantic similarity. The results are ranked using a cosine distance metric and link to related resources. It offers an interactive and efficient way to explore content using advanced embedding techniques.'
   },
   {
     name: 'Texte',
     url: 'blog/',
     Description:
-    'This page hosts a collection of blog posts or articles. It features a clean layout with a list of clickable titles, each leading to a specific blog post. Users can navigate through the posts and read detailed articles on various topics. The page ensures readability and accessibility for an engaging reading experience. AMOGUS',
+        'This page hosts a collection of blog posts or articles. It features a clean layout with a list of clickable titles, each leading to a specific blog post. Users can navigate through the posts and read detailed articles on various topics. The page ensures readability and accessibility for an engaging reading experience. AMOGUS',
     children: [
-      {name: 'Amogus', url: 'amogus/', Description: 'AMOGUS'},
+      {name: 'Amogus', url: 'amogus/', reload: true, Description: 'AMOGUS'},
       {name: 'Babel', url: 'babel/', Description: 'Babel 😳'},
     ]
   },
@@ -134,13 +135,13 @@ export const routes: Route[] = generateRoutes([
 export const path_routes: Map<string, Route> = generatePathRoutes(routes, '');
 
 function generatePathRoutes(
-  routes: Route[], parentPath: string): Map<string, Route> {
-    let pr = new Map<string, Route>();
-    for (const route of routes) {
-      const path = parentPath + route.url;
-      route.absoluteUrl = (base) => new URL(path, base).href;
-      pr.set(path, route);
-      if (route.children) {
+    routes: Route[], parentPath: string): Map<string, Route> {
+  let pr = new Map<string, Route>();
+  for (const route of routes) {
+    const path = parentPath + route.url;
+    route.absoluteUrl = (base) => new URL(path, base).href;
+    pr.set(path, route);
+    if (route.children) {
       pr = new Map<string, Route>(
           [...pr, ...generatePathRoutes(route.children, path)]);
     }
