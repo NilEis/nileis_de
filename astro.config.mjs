3996579e0b97f460c706ec376b7f4a6bd0fd46db
NilEis <52820950+NilEis@users.noreply.github.com>
2025-07-30 16:13:44 +0200

    add: not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import compressor from 'astro-compressor';
 import {defineConfig} from 'astro/config';
